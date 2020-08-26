2ea34b2f82bdfd30ba831231fee3fc00d5823502
reza-torabi-Reto <forecuont@gmail.com>
2020-08-26 13:41:58 +0430

    add validations and show error for login page

--- mysite/settings.py
+++ mysite/settings.py
@@ -28,7 +28,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',
     'account.apps.AccountConfig',
-    # 'jalali_date',
+    'widget_tweaks',
     'django_cleanup',
 ]
 
@@ -113,6 +113,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 # STATICFILES_DIRS = [
 #     os.path.join(BASE_DIR, 'static')
 # ]
